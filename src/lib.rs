a4ee0c9d67e4a16761894e49ff10f22a7ae53764
André Kristiansen <a@dev.bio>
2022-10-27 22:08:14 +0200

    Parallelization
    ...

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,17 @@
 //! }
 //! ```
 
-use std::time::{Duration};
+use std::{
+    
+    time::{Duration}, 
+    fmt::{Debug},
+};
+
+use rayon::prelude::{
+
+    IntoParallelIterator,
+    ParallelIterator,
+};
 
 use reqwest::blocking::{Client};
 
@@ -34,18 +44,23 @@ pub mod error;
 pub use certificate_log::{Entry};
 pub use error::{StreamError};
 
-pub struct StreamConfig<E: AsRef<str>> {
+#[derive(Debug, Clone)]
+pub struct StreamConfig<E>
+where E: AsRef<str> + Debug {
     pub endpoint: E,
     pub timeout: Option<Duration>,
+    pub workers: Option<usize>,
     pub index: Option<usize>,
 }
 
-impl <E: AsRef<str>> StreamConfig<E> {
+impl<E> StreamConfig<E> 
+where E: AsRef<str> + Debug {
     pub fn new(endpoint: E) -> Self {
         StreamConfig { 
 
             endpoint: endpoint, 
             timeout: Some(Duration::from_secs(1)),
+            workers: None,
             index: None, 
         }
     }
@@ -54,6 +69,16 @@ impl <E: AsRef<str>> StreamConfig<E> {
         StreamConfig { 
             endpoint: self.endpoint, 
             timeout: Some(timeout), 
+            workers: self.workers,
+            index: self.index,
+        }
+    }
+
+    pub fn workers(self, threads: usize) -> Self {
+        StreamConfig { 
+            endpoint: self.endpoint, 
+            timeout: self.timeout, 
+            workers: Some(threads.max(2)),
             index: self.index,
         }
     }
@@ -62,48 +87,121 @@ impl <E: AsRef<str>> StreamConfig<E> {
         StreamConfig { 
             endpoint: self.endpoint, 
             timeout: self.timeout, 
+            workers: self.workers,
             index: Some(index),
         }
     }
 }
 
-pub fn stream<E, F>(config : StreamConfig<E>, mut handler: F) -> Result<(), StreamError>
-where E: AsRef<str>, F: FnMut(Entry) -> bool {
+pub fn stream<E, F, R>(config : StreamConfig<E>, mut handler: F) -> Result<R, StreamError>
+where E: AsRef<str> + Debug, F: FnMut(Entry) -> Option<R> {
 
     let client = Client::new();
 
     let StreamConfig { 
         endpoint, 
         timeout, 
+        workers,
         index,
     } = config;
 
-    let mut current = certificate_log::get_log_size(&client, endpoint.as_ref())?;
+    let endpoint = endpoint.as_ref();
 
-    if let Some(index) = index {
-        current = if index < current { index } 
-            else { current }
-    }
-    
-    loop {
+    let (size, batch) = {
 
-        let entries = certificate_log::get_log_entries(&client, endpoint.as_ref(), current, 10000)?;
+        let size = certificate_log::get_log_size(&client, endpoint)?;
+        let batch = certificate_log::get_log_entries(&client, endpoint, 0, size)?.len();
 
-        if entries.is_empty() {
-            if let Some(timeout) = timeout {
-                std::thread::sleep(timeout);
+        (size, batch)
+    };
+
+    let mut position = if let Some(index) = index {
+        if index < size { index } else { size } 
+    } else { 0 };
+
+    if let Some(workers) = workers {
+        type WorkResult = Result<Vec<Entry>, StreamError>;
+
+        loop {
+
+            let collection: Vec<WorkResult> = (0..(workers)).into_par_iter()
+                .map(|offset| -> WorkResult {
+
+                    let start = position + (offset * batch);
+                    let mut collection = Vec::with_capacity(batch);
+                    
+                    loop {
+
+                        let start = start + collection.len();
+                        let count = batch - collection.len();
+
+                        let entries = certificate_log::get_log_entries(&client, endpoint, start, count)?;
+
+                        if entries.is_empty() {
+                            if let Some(timeout) = timeout {
+                                std::thread::sleep(timeout);
+                            }
+                        }
+
+                        else {
+
+                            collection.extend(entries);
+                            
+                            if collection.len() < batch { continue }
+                                else { return Ok(collection) }
+                        }
+                    }
+                }).collect();
+
+            for result in collection {
+                match result {
+
+                    Err(error) => {
+
+                        return Err(error)
+                    },
+
+                    Ok(entries) => {
+
+                        position = position + {
+                            entries.len()
+                        };
+
+                        for entry in entries {
+                            if let Some(result) = handler(entry) {
+                                return Ok(result)
+                            }
+                        }
+                    },
+                }
             }
         }
+    }
 
-        else {
+    else {
+    
+        loop {
 
-            current += entries.len();
+            let entries = certificate_log::get_log_entries(&client, endpoint, position, batch)?;
 
-            for entry in entries {
-                if handler(entry) { continue } else {
-                    return Ok(())
+            if entries.is_empty() {
+                if let Some(timeout) = timeout {
+                    std::thread::sleep(timeout);
                 }
-            }     
-        }       
+            }
+
+            else {
+
+                position = position + {
+                    entries.len()
+                };
+
+                for entry in entries {
+                    if let Some(result) = handler(entry) {
+                        return Ok(result)
+                    }
+                }  
+            }       
+        }
     }
 }
